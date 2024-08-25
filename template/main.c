38a67cdd24fef78ae9a6a3fb9b5a861c7f9caed6
achieve_dream <wulei7217@gmail.com>
2024-08-25 13:11:41 +0800

    ```txtfeat(template): 设置构建类型为Debug并配置烧录软件路径
    在CMakeLists.txt中设置了构建类型为Debug模式，并指定了烧录软件的路径。
    同时，对main.c文件中的LED1定义进行了清理，确保P1SEL的正确配置。
    
    /ᐠ・ꞈ・ᐟ\ by Jun's Sublime Text 3
    ```

--- template/main.c
+++ template/main.c
@@ -1,12 +1,11 @@
 #include "ioCC2530.h"
-
 #define LED1 P1_0
 
 typedef unsigned int uint8;
 
 void initIO() {
     P1DIR |= 0x01;
-    P1SEL &= ~0x01; 
+    P1SEL &= ~0x01;
 }
 
 void delayMs(uint8 ms) {
